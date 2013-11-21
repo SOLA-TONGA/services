09bdb7f8f9aee8bbfcc01f639589dcc63d9329d0
amcdowell <armcdowell@hotmail.com>
2013-11-22 09:16:38 +1300

    Added JXTreeTable for displaying service list

--- ejbs/application/src/main/java/org/sola/services/ejb/application/repository/entities/RequestType.java
+++ ejbs/application/src/main/java/org/sola/services/ejb/application/repository/entities/RequestType.java
@@ -1,28 +1,30 @@
 /**
  * ******************************************************************************************
- * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations (FAO).
- * All rights reserved.
+ * Copyright (C) 2012 - Food and Agriculture Organization of the United Nations
+ * (FAO). All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *    1. Redistributions of source code must retain the above copyright notice,this list
- *       of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright notice,this list
- *       of conditions and the following disclaimer in the documentation and/or other
- *       materials provided with the distribution.
- *    3. Neither the name of FAO nor the names of its contributors may be used to endorse or
- *       promote products derived from this software without specific prior written permission.
+ * 1. Redistributions of source code must retain the above copyright notice,this
+ * list of conditions and the following disclaimer. 2. Redistributions in binary
+ * form must reproduce the above copyright notice,this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3. Neither the name of FAO nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT,STRICT LIABILITY,OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * *********************************************************************************************
  */
 /*
@@ -37,21 +39,22 @@ import javax.persistence.Column;
 import javax.persistence.Table;
 import org.sola.services.common.repository.ChildEntityList;
 import org.sola.services.common.repository.DefaultSorter;
+import org.sola.services.common.repository.Localized;
 import org.sola.services.common.repository.entities.AbstractCodeEntity;
 
 /**
- *  Entity representing the application.request_type code table. This code entity includes
- *  some additional fields beyond the standard code, description, display_value and status used
- *  for most code entities. 
+ * Entity representing the application.request_type code table. This code entity
+ * includes some additional fields beyond the standard code, description,
+ * display_value and status used for most code entities.
+ *
  * @author soladev
  */
 @Table(name = "request_type", schema = "application")
-@DefaultSorter(sortString="display_value")
+@DefaultSorter(sortString = "display_order")
 public class RequestType extends AbstractCodeEntity {
-    
+
     public static String CADASTRE_CHANGE = "cadastreChange";
     public static String SURVEY = "survey";
-
     @Column(name = "nr_days_to_complete")
     private int nrDaysToComplete;
     @Column(name = "request_category_code")
@@ -70,6 +73,11 @@ public class RequestType extends AbstractCodeEntity {
     private String typeActionCode;
     @Column(name = "rrr_type_code")
     private String rrrTypeCode;
+    @Column(name = "display_order")
+    private int displayOrder;
+    @Localized
+    @Column(name = "display_group_name")
+    private String displayGroupName;
     @ChildEntityList(parentIdField = "requestTypeCode")
     private List<RequestTypeRequiresSourceType> sourceTypeCodes;
 
@@ -81,7 +89,7 @@ public class RequestType extends AbstractCodeEntity {
         return areaBaseFee;
     }
 
-    public void setAreaBaseFee(BigDecimal areaBaseFee) { 
+    public void setAreaBaseFee(BigDecimal areaBaseFee) {
         this.areaBaseFee = areaBaseFee;
     }
 
@@ -121,7 +129,7 @@ public class RequestType extends AbstractCodeEntity {
         return requestCategoryCode;
     }
 
-    public void setRequestCategoryCode(String requestCategoryCode) { 
+    public void setRequestCategoryCode(String requestCategoryCode) {
         this.requestCategoryCode = requestCategoryCode;
     }
 
@@ -155,5 +163,21 @@ public class RequestType extends AbstractCodeEntity {
 
     public void setSourceTypeCodes(List<RequestTypeRequiresSourceType> sourceTypeCodes) {
         this.sourceTypeCodes = sourceTypeCodes;
+    }
+
+    public int getDisplayOrder() {
+        return displayOrder;
+    }
+
+    public void setDisplayOrder(int displayOrder) {
+        this.displayOrder = displayOrder;
+    }
+
+    public String getDisplayGroupName() {
+        return displayGroupName;
+    }
+
+    public void setDisplayGroupName(String displayGroupName) {
+        this.displayGroupName = displayGroupName;
     }
 }
